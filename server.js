945bced0b878300c37e855c517f7466f65107a0e
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-09-01 18:06:45 +0530

    updated.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from CI/CD with GitHub Actions 🚀");
+  res.send("Hello from CI/CD with GitHub Actions updated 🚀");
 });
 
 app.listen(PORT, () => {
