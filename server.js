c4a61fc9158d1ac2739c8093825bc32c9a65f5cb
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-09-01 17:43:01 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from CI/CD with GitHub Actions 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running`);
+});
